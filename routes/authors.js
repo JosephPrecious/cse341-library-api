198706f169bfd0b63e4ecccdafa9fa386f1ce735
JosephPrecious <josephprecious419@gmail.com>
2026-05-27 11:00:16 +0100

    feat: refactor author routes and improve Swagger documentation

--- routes/authors.js
+++ routes/authors.js
@@ -3,58 +3,40 @@ const mongodb = require("../data/database");
 const router = express.Router();
 const ObjectId = require("mongodb").ObjectId;
 
-/**
- * @swagger
- * /authors:
- *   get:
- *     summary: Get all authors
- *     responses:
- *       200:
- *         description: Success
- */
+/* =========================
+   GET ALL AUTHORS
+========================= */
 router.get("/", async (req, res) => {
   try {
     const db = mongodb.getDb();
-
-    const result = await db.collection("authors").find().toArray();
-
-    res.status(200).json(result);
+    const authors = await db.collection("authors").find().toArray();
+    res.status(200).json(authors);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 });
 
-/**
- * @swagger
- * /authors/{id}:
- *   get:
- *     summary: Get one author
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Success
- */
+/* =========================
+   GET ONE AUTHOR
+========================= */
 router.get("/:id", async (req, res) => {
   try {
     const db = mongodb.getDb();
-
     const authorId = new ObjectId(req.params.id);
 
-    const result = await db
+    const author = await db
       .collection("authors")
       .findOne({ _id: authorId });
 
-    res.status(200).json(result);
+    res.status(200).json(author);
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 });
 
+/* =========================
+   CREATE AUTHOR
+========================= */
 /**
  * @swagger
  * /authors:
@@ -62,96 +44,65 @@ router.get("/:id", async (req, res) => {
  *     summary: Create an author
  *     requestBody:
  *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - nationality
+ *               - age
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               nationality:
+ *                 type: string
+ *               age:
+ *                 type: number
  *     responses:
  *       201:
  *         description: Created
+ *       400:
+ *         description: Missing fields
  */
 router.post("/", async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      nationality,
-      age
-    } = req.body;
+    const { firstName, lastName, nationality, age } = req.body;
 
-    if (
-      !firstName ||
-      !lastName ||
-      !nationality ||
-      !age
-    ) {
-      return res.status(400).json({
-        message: "All fields are required"
-      });
+    if (!firstName || !lastName || !nationality || !age) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     const db = mongodb.getDb();
 
-    const author = {
-      firstName,
-      lastName,
-      nationality,
-      age
-    };
+    const newAuthor = { firstName, lastName, nationality, age };
 
-    const response = await db
-      .collection("authors")
-      .insertOne(author);
+    const result = await db.collection("authors").insertOne(newAuthor);
 
-    res.status(201).json({
-      id: response.insertedId
-    });
+    res.status(201).json({ id: result.insertedId });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
 });
 
-/**
- * @swagger
- * /authors/{id}:
- *   put:
- *     summary: Update an author
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       204:
- *         description: Updated
- */
+/* =========================
+   UPDATE AUTHOR
+========================= */
 router.put("/:id", async (req, res) => {
   try {
-    const {
-      firstName,
-      lastName,
-      nationality,
-      age
-    } = req.body;
-
-    if (
-      !firstName ||
-      !lastName ||
-      !nationality ||
-      !age
-    ) {
-      return res.status(400).json({
-        message: "All fields are required"
-      });
-    }
-
     const db = mongodb.getDb();
-
     const authorId = new ObjectId(req.params.id);
 
-    const updatedAuthor = {
-      firstName,
-      lastName,
-      nationality,
-      age
-    };
+    const { firstName, lastName, nationality, age } = req.body;
+
+    if (!firstName || !lastName || !nationality || !age) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const updatedAuthor = { firstName, lastName, nationality, age };
 
     await db.collection("authors").replaceOne(
       { _id: authorId },
@@ -164,32 +115,17 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-/**
- * @swagger
- * /authors/{id}:
- *   delete:
- *     summary: Delete an author
- *     parameters:
- *       - in: path
- *         name: id
- *         required: true
- *         schema:
- *           type: string
- *     responses:
- *       200:
- *         description: Deleted
- */
+/* =========================
+   DELETE AUTHOR
+========================= */
 router.delete("/:id", async (req, res) => {
   try {
     const db = mongodb.getDb();
-
     const authorId = new ObjectId(req.params.id);
 
-    await db.collection("authors").deleteOne({
-      _id: authorId
-    });
+    await db.collection("authors").deleteOne({ _id: authorId });
 
-    res.status(200).send();
+    res.status(200).json({ message: "Deleted successfully" });
   } catch (err) {
     res.status(500).json({ message: "Server error" });
   }
